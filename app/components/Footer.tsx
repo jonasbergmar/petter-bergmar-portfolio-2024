996ad0b5c3996279c57d976bc806a272e13c0229
jonasbergmar <jonas.bergmar@hotmail.com>
2024-09-13 11:07:20 +0200

    First

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="container h-96 border">Footer</div>;
+};
+
+export default Footer;
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="container h-svh w-full border">Homeome</div>;
+};
+
+export default Home;
